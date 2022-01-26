187ec59ecdd2ebbf7e8c84a510a97b808413f9bb
Danilo Falcirolli <danilo.falcirolli5531@gmail.com>
2022-01-26 08:29:15 -0300

    update

--- inventario/index.js
+++ inventario/index.js
@@ -4,53 +4,73 @@ function init(){
 }
 
 function addEvent(){
+    document.querySelector('#type').addEventListener('click',()=>{
+        trocarBusca();
+    })
     document.querySelector("#organizador").addEventListener("change", ()=>{
         let item = window.location.href.split("?");
         window.location.href = item[0]+"?org="+document.querySelector("#organizador").value;
     })
     document.querySelector("#procurar").addEventListener("keyup",()=>{
-        if(document.querySelector("#procurar").value == ''){
-            let tr = document.querySelectorAll('tr')
-            tr.forEach((e,index)=> {
-                if(index > 1){
-                    e.style = "display:table-row"
-                }
-            })
-        }else{
-            buscar();
-        }
+        selecionar(document.querySelector('#type').classList);
     })
 }
 
-function buscar() {
-    let tr = document.querySelectorAll('tr');
-    tr.forEach((e,index)=> {
-        if(index > 1){
-            e.style = "display:table-row";
+function trocarBusca(){
+    if(document.querySelector('#type').classList.contains('orange')){
+        document.querySelector('#type').classList.remove('orange');
+        document.querySelector('#type').classList.toggle('purple');
+        document.querySelector("#procurar").type = 'text'
+    }else{
+        document.querySelector('#type').classList.remove('purple');
+        document.querySelector('#type').classList.toggle('orange');
+        document.querySelector("#procurar").type = 'number'
+    }
+}
+
+function selecionar(element) {
+    if(document.querySelector('#type').classList.contains('orange')){
+        buscar(0)
+    }else{
+        buscar(1)
+    }
+}
+function buscar(n){
+    let tr = document.querySelectorAll('tr')
+    tr.forEach((e,index) => {
+        if(index>1){
+            e.style = 'display:table-row'
         }
     })
-    tr.forEach((e,index)=> {
-        if(index > 1){
-            if(!e.querySelectorAll('td')[0].innerHTML.includes(document.querySelector("#procurar").value)){
-                e.style = "display:none";
+    if(n==0){
+        tr.forEach((e,index)=>{
+            if(index>1){
+                if(!e.querySelectorAll('td')[n].innerHTML.includes(document.querySelector("#procurar").value)){
+                    e.style = 'display:none'
+                }
             }
-        }
-    })
+        })
+    }else{
+        tr.forEach((e,index)=>{
+            if(index>1){
+                if(!e.querySelectorAll('td')[n].innerHTML.toLocaleLowerCase().includes(document.querySelector("#procurar").value.toLocaleLowerCase())){
+                    e.style = 'display:none'
+                }
+            }
+        })
+    }
 }
 
 async function org(){
-
     var resp;
     async function connect(){
         var item = await fetch("https://gerenciamento-estoque-senai.herokuapp.com/itens");
         resp = await item.json();
         return resp;
     }
-
     await connect();
     let organizador = document.querySelector('#organizador');
     var params = new URLSearchParams(window.location.search);
-
     if(params.get("org")=="ni"){
         organizador.value = params.get("org");
         let item = resp.sort((x,y)=>{
@@ -76,24 +96,20 @@ async function org(){
         organizador.value = "";
         listartodos(resp);
     }
-
 }
 
 function listartodos(items){
     var infos = document.querySelector('#informacoes');
     for(let a = 0; a != items.length; a++){
         const model = document.querySelector('.model').cloneNode(true);
-
         model.querySelector('#ni').innerHTML = items[a].codigoNI;
         model.querySelector('#descricao').innerHTML = items[a].nome;
         model.querySelector('#status').innerHTML = items[a].status;
         model.querySelector('a').href = `../item/index.html?id=${items[a].id}`;
-        model.classList.remove('model');
-
+        model.classList.remove('model')
         if(a%2 == 1){
             model.classList.toggle('gray');
         }
-
         infos.appendChild(model);
     }
 }
